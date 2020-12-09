cebc960530ae5e6f01b197389c7ebfc07dc7faff
Yrjan van Wijk <yrjan@live.nl>
2020-12-09 15:17:56 +0100

    add day9

---
+++ day9/day9-part1.py
@@ -0,0 +1,61 @@
+import sys
+import fileinput
+from re import search
+import time
+startTime = time.time()
+
+inputData = []
+preambleSize = 25
+preambleData = []
+invalidEntry = 0
+for line in fileinput.input("./input.txt"):
+    cleanLine = line.encode("utf-8").strip("\n")
+    # print(cleanLine)
+    inputData.append(int(cleanLine))
+
+
+for i in range(len(inputData)):
+    if i < preambleSize:
+        preambleData.append(inputData[i])
+    else:
+        found = False
+        for numbers in preambleData:
+            remain = inputData[i] - numbers
+            # print(remain)
+            if preambleData.count(remain) == 1:
+                # print("OK")
+                found = True
+                pass
+        if not found:
+            invalidEntry = inputData[i]
+            break;
+        preambleData.pop(0)
+        preambleData.append(inputData[i])
+
+print(str(invalidEntry) + " is not confirm preamble")
+print("--- part1: %s seconds ---" % (time.time() - startTime))
+
+for i in range(len(inputData)):
+    continousValues= []
+    sum = 0
+    offset = 0
+    while sum < invalidEntry:
+        sum += inputData[i+offset]
+        continousValues.append(inputData[i+offset])
+        # print(inputData[i+offset])
+        offset +=1
+    if sum == invalidEntry and len(continousValues)> 1:
+        print(sum)
+        continousValues.sort()
+        print(str(continousValues[0]) + " is lowest value")
+        print(str(continousValues[-1]) + " is highest value")
+        print("result: " + str(continousValues[0] + continousValues[-1]))
+        print(str(continousValues) + " is list")
+        pass
+    elif sum < invalidEntry:
+        print(continousValues)
+        print(len(continousValues))
+    # print(continousValues)
+
+# print(str(invalidEntry) + " is not confirm preamble")
+print("--- part2: %s seconds ---" % (time.time() - startTime))
--- dayX-part1.py
+++ dayX-part1.py
@@ -1,6 +1,10 @@
 import sys
 import fileinput
 from re import search
-
+import time
+startTime = time.time()
 for line in fileinput.input("./input7.txt"):
     print(line)
+
+
+print("--- %s seconds ---" % (time.time() - startTime))
